c9d4c1f4e8275dec9d5eb4f8585f7c266fb32245
AhmetSelimPehlivan <ahmetselimpehlivan@gmail.com>
2023-04-26 00:36:24 +0300

    s

--- backend/firebase_connection.js
+++ backend/firebase_connection.js
@@ -1,13 +1,13 @@
 const fireabase = require('firebase')
 
 fireabase_conn = fireabase.initializeApp({
-    apiKey: "AIzaSyBZtlRxIvjFsO8xIWpsBwCTzM6WzPuWeJw",
-    authDomain: "meta-school-ae3cc.firebaseapp.com",
-    databaseURL: "https://meta-school-ae3cc-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "meta-school-ae3cc",
-    storageBucket: "meta-school-ae3cc.appspot.com",
-    messagingSenderId: "84657342772",
-    appId: "1:84657342772:web:4f3d3ae3875f2fb58abd61"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.SENDERID,
+    appId: process.env.APPID
   });
 
 module.exports = fireabase_conn;
