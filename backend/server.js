c9a5202956080dad240dccc24a22698ef6db6411
AhmetSelimPehlivan <ahmetselimpehlivan@gmail.com>
2023-04-27 02:40:24 +0300

    a

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,6 @@ app.use(authRoute);
 app.use(taskRoute);
 
 // --------------------------deployment------------------------------
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*',(req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend','build','index.html'))
-})
+
 // --------------------------deployment------------------------------
 server.listen(process.env.PORT || 8080)
